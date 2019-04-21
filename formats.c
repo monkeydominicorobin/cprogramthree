b451a990aa70eae8dd97db94f3dfbedcd3d145af
Rommel Dominguez <leonidastouch@icloud.com>
2019-04-21 22:56:39 +0800

    exploring the mystery behind format specifiers

---
+++ formats.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	float a = 10.1;
+	//bool b = false;
+	printf("%a\n",a);
+	printf("%e\n",a);
+	printf("%s\n","false");
+	printf("%c\n",'t');
+	printf("%f\n",a);
+	return 0;
+}
